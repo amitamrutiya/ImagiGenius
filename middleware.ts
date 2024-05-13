9bda6fdd5450427b873e95192fc793635088f64b
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-13 10:06:56 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  ignoredRoutes: ["/api/webhooks/clerk"],
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
