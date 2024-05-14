1d10d4771b4cd2f0edf03334446e34ad81cfbe42
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-14 11:10:12 +0530

    add cloudinary add create media uploader form
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -31,6 +31,8 @@ import {
 } from "@/components/ui/select";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TranformedImage";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -220,6 +222,33 @@ const TransformationForm = ({
             )}
           </div>
         )}
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
+          />
+        </div>
+
+
         <div className="flex flex-col gap-4">
           <Button
             type="button"
